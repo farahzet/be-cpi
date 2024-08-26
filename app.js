f94fb32bac4d88cef6e118a57e8d68c245d8cb8d
farahzet <119497241+farahzet@users.noreply.github.com>
2024-08-26 17:56:43 +0700

    add true app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use(router);
