f91e85d4f5130590dd00fc5de0a8ac59714f8ca6
farahzet <119497241+farahzet@users.noreply.github.com>
2024-08-26 16:56:19 +0700

    add server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on: localhost:${PORT}`);
