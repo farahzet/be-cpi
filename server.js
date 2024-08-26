79fb45b1ac952112e31af26f3685bebcc03fcf1a
farahzet <119497241+farahzet@users.noreply.github.com>
2024-08-26 16:30:23 +0700

    add pool server

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const { Pool } = require('pg');
 const app = require("./app");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 const PORT = process.env.PORT;
